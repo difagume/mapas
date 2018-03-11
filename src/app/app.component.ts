15799b5d27ecd4ce91e986a7449ed69595c04203
Diego Fabricio <difagume@gmail.com>
2018-03-11 17:29:44 -0500

    Eventos en el Mapa - Crear marcadores, arrastrarlos y obtenr coordenadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapaService } from './services/mapa.service';
+import { Marcador } from './interfaces/marcador.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,22 @@ export class AppComponent {
   lat: number = -0.0777478;
   lng: number = -78.469511;
   zoom: number = 16;
+
+  constructor(public _ms: MapaService) {
+
+  }
+
+  clickMapa(evento) {
+    console.log('evento: ', evento);
+
+    let nuevoMarcador: Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: 'Sin título',
+      draggable: true
+    }
+
+    this._ms.insertarMarcador(nuevoMarcador);
+  }
+
 }
--- src/app/services/mapa.service.ts
+++ src/app/services/mapa.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from '../interfaces/marcador.interface';
 
 @Injectable()
 export class MapaService {
 
-  constructor() { }
+  marcadores: Marcador[] = [];
+
+  constructor() {
+    let nuevoMarcador: Marcador = {
+      lat: -0.0777478,
+      lng: -78.469511,
+      titulo: 'La Jose',
+      draggable: true
+    };
+
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  insertarMarcador(marcador: Marcador) {
+    this.marcadores.push(marcador);
+  }
 
 }
